972f8348a632c5e3f3f54cae60663a234e15b727
Baptiste <baptiste.miquel@viacesi.fr>
2019-05-26 14:35:35 +0200

    BDD connection

--- JPU-BlankProject/model/src/main/java/model/bdd/BDDConnector.java
+++ JPU-BlankProject/model/src/main/java/model/bdd/BDDConnector.java
@@ -4,4 +4,34 @@ import java.sql.*;
 
 public class BDDConnector {
 
+	private static String BDDNAME = "boulderdash";
+	private static String URL = "jdbc:mysql://localhost/" + BDDNAME
+			+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false";
+	private static String USER = "root";
+	private static String PASSWORD = "";
+	
+	static BDDConnector INSTANCE;
+	private Connection connection;
+
+	// Singleton BDDConnector
+	public BDDConnector getInstance() {
+		if (INSTANCE != null) {
+			INSTANCE = new BDDConnector();
+		}
+		return INSTANCE;
+	}
+
+	public void openBDD() {
+		try {
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println("Successfully connected to database " + BDDNAME);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
 }
--- JPU-BlankProject/model/src/main/java/model/bdd/BDDGetData.java
+++ JPU-BlankProject/model/src/main/java/model/bdd/BDDGetData.java
@@ -1,5 +1,37 @@
 package model.bdd;
 
-public class BDDGetData {
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class BDDGetData {
+	private BDDConnector bddConnector;
+	CallableStatement state;
+	
+	public BDDGetData() {
+		this.bddConnector = new BDDConnector();
+		this.bddConnector.openBDD();
+	}
+	
+	public void getLevel(String id) {
+		
+	    try {
+			CallableStatement state = bddConnector.getConnection().prepareCall("{call GET_MAP_FROM_ID(?)}");
+			state.setString(1, id);
+			state.execute();
+			
+			ResultSet result = state.getResultSet();
+			while(result.next()) {
+				System.out.println("Loaded map:");
+				System.out.println(result.getString("content"));
+			}
+			result.close();
+			state.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
