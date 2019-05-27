c8feaa2ca1d4e6f3140d5c737fd0462c6321f047
Baptiste <baptiste.miquel@viacesi.fr>
2019-05-27 15:00:28 +0200

    change map id

--- JPU-BlankProject/model/src/main/java/model/Model.java
+++ JPU-BlankProject/model/src/main/java/model/Model.java
@@ -26,7 +26,7 @@ public final class Model extends Observable implements IModel {
 		this.helloWorld = new HelloWorld();
 		this.map = new Map();
 		bdd = new BDDGetData();
-        bdd.loadLevel("1", map);
+        bdd.loadLevel("3", map);
 	}
 
 	public HelloWorld getHelloWorld() {
