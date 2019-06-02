26f3cfda3a7e2d24f7b673fb507d0c68eae8782d
Baptiste <baptiste.miquel@viacesi.fr>
2019-06-02 16:03:45 +0200

    Javadoc main

--- JPU-BlankProject/main/src/main/java/main/Main.java
+++ JPU-BlankProject/main/src/main/java/main/Main.java
@@ -1,5 +1,7 @@
 /**
- * @author Jean-Aymeric DIET jadiet@cesi.fr
+ * @author Florian Rossi florian.rossi@viacesi.fr
+ * @author Baptiste Miquel baptiste.miquel@viacesi.fr
+ * 
  * @version 1.0
  */
 package main;
@@ -9,24 +11,23 @@ import model.Model;
 import view.View;
 
 /**
- * The Class Main.
- *
- * @author Jean-Aymeric Diet
+ * The Class Main
+ * 
+ * @author Florian Rossi
+ * @author Baptiste Miquel
  */
 public abstract class Main {
 
-    /**
-     * The main method.
-     *
-     * @param args
-     *            the arguments
-     * @throws Exception 
-     */
-    public static void main(final String[] args) throws Exception {
-        final Model model = new Model();
-        final View view = new View(model);
-        final Controller controller = new Controller(view, model);
-        view.setController(controller);
-        System.out.println("test");
-    }
+	/**
+	 * The main method
+	 *
+	 * @param args The arguments
+	 * @throws Exception
+	 */
+	public static void main(final String[] args) throws Exception {
+		final Model model = new Model();
+		final View view = new View(model);
+		final Controller controller = new Controller(view, model);
+		view.setController(controller);
+	}
 }
